3b441d05e808394a3bdf868e9ca06d3a5530dd39
Sergio Shambir <realistic.sergio@gmail.com>
2025-12-25 22:03:08 +0100

    fix js file

--- JS/task-2.js
+++ JS/task-2.js
@@ -1,5 +1,6 @@
-function getShippingMessage(pacountry,price,deliveryFee) {
-    
+function getShippingMessage(country, price, deliveryFee) {
+    const totalPrice = price + deliveryFee;
+    return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
 
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
